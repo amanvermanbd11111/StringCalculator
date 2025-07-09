0987b1c799e988b352a808533a84397047305284
aman-berkowits <amanvermanbd11111@gmail.com>
2025-07-09 19:27:43 +0530

    test custom delimeter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -21,7 +21,10 @@ void main() {
     test('Handle new lines between numbers', () {
       expect(add('1\n2,3'), equals(6));
     });
-    
+
+    test('Custom delimiter ;', () {
+      expect(add('//;\n1;2'), equals(3));
+    });
 
   });
 }
